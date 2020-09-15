69f02baf2e45ebb6b29328bf00934d7320130db8
Alex De Sousa <alexndesousa@hotmail.co.uk>
2020-09-16 00:07:14 +0100

    Added objects for playlists and songs

---
+++ lib/models/playlist.dart
@@ -0,0 +1,11 @@
+
+class Playlist {
+  final String artworkUrl;
+  final String name;
+  final String description;
+  final String id;
+
+  Playlist(this.artworkUrl, this.name, this.description, this.id);
+
+  //consider overiding hashCode and the == operator
+}
---
+++ lib/models/song.dart
@@ -0,0 +1,9 @@
+
+class Song {
+  final String id;
+  final String artist;
+  final String name;
+  final String artworkUrl;
+
+  Song(this.id, this.artist, this.name, this.artworkUrl);
+}
